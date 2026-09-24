b76812cff503de2455e80b9aa1ae2faf9c927131
shahin6452 <shahin.shohag6452@gmail.com>
2026-09-25 02:00:41 +0600

    hero section

---
+++ src/app/components/Library.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Library = () => {
+    return (
+        <div>
+            <div className='container'>
+                <h2>THE LIBRARY</h2>
+                <p>Twelve lifts covering every major muscle group.</p>
+
+            </div>
+        </div>
+    )
+};
+
+export default Library;
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import HeroSection from "./components/Hero";
+import Library from "./components/Library";
 
 export default function Home() {
 
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <div>
       <HeroSection></HeroSection>
+      <Library></Library>
     </div>
     
   );
